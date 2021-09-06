27098a9193bc476eaf7371b4e1b1a11a25f356a7
Gavin Wu <gavin.wu@bontouch.com>
2021-09-06 16:16:49 +0100

    Set up initial Scrumdinger classes

---
+++ ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Gavin Wu on 06/09/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
