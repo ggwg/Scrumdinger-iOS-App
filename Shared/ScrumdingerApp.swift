b79a88571e717e844aeb805e2caa59cf1f1aaa5f
Gavin Wu <gavin.wu@bontouch.com>
2021-09-06 14:58:07 +0100

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Gavin Wu on 06/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
